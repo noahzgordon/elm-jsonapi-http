bf484e5243fe3d904ae840a25f32770a95b2b036
Noah Gordon <noahzacharygordon@gmail.com>
2016-08-29 14:35:37 -0400

    Update example for use with example server

--- example/Main.elm
+++ example/Main.elm
@@ -2,10 +2,16 @@ module Main exposing (main)
 
 import Html
 import Html.App
+import Http
+import Task
 import Html.Events exposing (onClick)
 import Platform.Cmd exposing ((!))
 import Platform.Sub exposing (Sub)
+import Json.Decode exposing ((:=))
+import JsonApi
+import JsonApi.Resources
 import JsonApi.Http
+import Debug
 
 
 main =
@@ -19,6 +25,8 @@ main =
 
 type Message
     = GetInitialModel
+    | ProtagonistLoaded JsonApi.Resource
+    | ProtagonistFailedToLoad Http.Error
 
 
 view model =
@@ -33,14 +41,38 @@ renderProtagonist model =
         Nothing ->
             Html.button [ Html.Events.onClick GetInitialModel ] [ Html.text "Get Initial Model" ]
 
-        Just user ->
-            Html.p [] [ Html.text (user.firstName ++ " " ++ user.lastName) ]
+        Just character ->
+            Html.p [] [ Html.text (character.firstName ++ " " ++ character.lastName) ]
 
 
 update message model =
     case message of
         GetInitialModel ->
-            model ! [getProtagonist]
+            model ! [ getProtagonist ]
+
+        ProtagonistLoaded resource ->
+            { model | protagonist = JsonApi.Resources.attributes characterDecoder resource |> Result.toMaybe } ! []
+
+        ProtagonistFailedToLoad e ->
+            Debug.log ("Remember to start the server on port 9292! " ++ toString e) (model ! [])
+
+
+type alias Model =
+    { protagonist : Maybe Character
+    }
+
+
+type alias Character =
+    { firstName : String
+    , lastName : String
+    }
+
+
+characterDecoder : Json.Decode.Decoder Character
+characterDecoder =
+    Json.Decode.object2 Character
+        ("first-name" := Json.Decode.string)
+        ("last-name" := Json.Decode.string)
 
 
 initialModel =
@@ -50,4 +82,5 @@ initialModel =
 
 getProtagonist : Cmd Message
 getProtagonist =
-    JsonApi.Http.get "localhost:3000"
+    JsonApi.Http.getPrimaryResource "http://localhost:9292/luke"
+        |> Task.perform ProtagonistFailedToLoad ProtagonistLoaded
